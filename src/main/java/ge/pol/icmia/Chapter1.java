870bc4890465b2b98df0c3db310fb2ac37022f2d
Vako Beridze <vako.beridze@gmail.com>
2014-11-27 20:56:10 +0400

    created project

---
+++ src/main/java/ge/pol/icmia/Chapter1.java
@@ -0,0 +1,48 @@
+package ge.pol.icmia;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by vako on 11/27/14.
+ */
+public class Chapter1 {
+    /**
+     * Path to the resulting PDF file.
+     */
+    public static final String RESULT = "results/chapter1/hello.pdf";
+
+    /**
+     * Creates a PDF file: hello.pdf
+     *
+     * @param args no arguments needed
+     */
+    public static void main(String[] args) throws DocumentException, IOException {
+        new Chapter1().createPdf(RESULT);
+    }
+
+    /**
+     * Creates a PDF document.
+     *
+     * @param filename the path to the new PDF document
+     * @throws DocumentException
+     * @throws IOException
+     */
+    public void createPdf(String filename) throws DocumentException, IOException {
+        // step 1
+        Document document = new Document();
+        // step 2
+        PdfWriter.getInstance(document, new FileOutputStream(filename));
+        // step 3
+        document.open();
+        // step 4
+        document.add(new Paragraph("Hello World!"));
+        // step 5
+        document.close();
+    }
+}
